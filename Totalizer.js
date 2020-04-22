4d4c6cccf71a6271a53f267ca51e8815d97cc50c
Jason Zhang <zjjc123@hotmail.com>
2020-04-21 23:44:38 -0700

    Trimming White Spaces
    Now duplication only occurs if people misspell their own names somehow or add extra information

--- Totalizer.js
+++ Totalizer.js
@@ -12,7 +12,8 @@ function Totalize() {
         names.push(String(cell).toLowerCase()
             .split(' ')
             .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
-            .join(' '));
+            .join(' ')
+            .trim());
     }
 
     // Get all hours
